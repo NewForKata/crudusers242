6efe6d3494fe497a3afbb0f751fe9b543a83509e
IYudashkin <yudashkin.work@gmail.com>
2021-12-20 14:13:33 +0400

    Доавил транзакции

--- crudusers242/src/main/java/web/service/UserServiceImpl.java
+++ crudusers242/src/main/java/web/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.dao.RoleDAO;
 import web.dao.UserDao;
@@ -20,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService, UserDetailsService {
     private UserDao userDAO;
     private RoleDAO roleDAO;
@@ -27,37 +29,44 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Autowired
     PasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional
     @Autowired
     public void setUserDAO(UserDao userDAO) {
         this.userDAO = userDAO;
     }
 
+    @Transactional
     @Autowired
     public void setRoleDAO(RoleDAO roleDAO) {
         this.roleDAO = roleDAO;
     }
 
+    @Transactional
     @Override
     public List<User> allUsers() {
         return userDAO.allUsers();
     }
 
+    @Transactional
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userDAO.save(user);
     }
 
+    @Transactional
     @Override
     public void delete(User user) {
         userDAO.delete(user);
     }
 
+    @Transactional
     @Override
     public User getById(Long id) {
         return userDAO.getById(id);
     }
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) {
         User user = userDAO.getUserByName(username);
